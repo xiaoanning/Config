6ba20d59437c20d7fe12b11497feef10fcf9b75c
xiaoanning <734871221@qq.com>
2017-04-28 10:31:35 +0800

    1

---
+++ Config/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Config
+//
+//  Created by 安宁 on 2017/4/28.
+//  Copyright © 2017年 安宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
